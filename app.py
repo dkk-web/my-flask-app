207ae314d9a71494185eb8a5add79ed1aef21038
dkk-web <dkksbejeke@gmail.com>
2026-09-15 16:36:42 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__) #__name__代表目前执行的模组
+
+@app.route("/") # 函式的装饰（Decorator）
+def home():
+    return "Hello Flask 2"
+@app.route('/test') #代表我们要处理的网站路径
+def text():
+    return "This is Tast"
+
+if __name__ == "__main__": # 如果以主程式执行
+    app.run() #立刻启动服务器
