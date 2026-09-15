056c0440d7bb811517edb741bec6668837fbe01e
dkk-web <dkksbejeke@gmail.com>
2026-09-15 17:03:48 +0800

    update text

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__) #__name__代表目前执行的模组
 
 @app.route("/") # 函式的装饰（Decorator）
 def home():
-    return "Hello Flask 2"
+    return "凯哥牛逼"
 @app.route('/test') #代表我们要处理的网站路径
 def text():
     return "This is Tast"
